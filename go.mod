d0b60265d04b7b74eb78299335a01d9885c1165b
the-arcade-01 <aashisharcade@gmail.com>
2023-09-03 15:23:37 +0530

    mysql added

--- go.mod
+++ go.mod
@@ -3,3 +3,7 @@ module go-api
 go 1.20
 
 require github.com/go-chi/chi/v5 v5.0.10
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-sql-driver/mysql v1.7.1
--- main.go
+++ main.go
@@ -1,20 +1,54 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
+
+var (
+	DATABASE_URL, DB_DRIVER, PORT string
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Couldn't load env on startup!!")
+	}
+	DATABASE_URL = os.Getenv("DATABASE_URL")
+	DB_DRIVER = os.Getenv("DB_DRIVER")
+	PORT = os.Getenv("PORT")
+}
+
+func DBClient() (*sql.DB, error) {
+	db, err := sql.Open(DB_DRIVER, DATABASE_URL)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	fmt.Println("Connected to DB")
+	return db, nil
+}
 
 type Server struct {
 	Router *chi.Mux
+	DB     *sql.DB
 }
 
-func CreateServer() *Server {
+func CreateServer(db *sql.DB) *Server {
 	server := &Server{
 		Router: chi.NewRouter(),
+		DB:     db,
 	}
 	return server
 }
@@ -28,41 +62,94 @@ func (server *Server) MountHandlers() {
 
 	todosRouter := chi.NewRouter()
 	todosRouter.Group(func(r chi.Router) {
-		r.Get("/", GetTodos)
-		r.Post("/", AddTodo)
+		r.Get("/", server.GetTodos)
+		r.Post("/", server.AddTodo)
 	})
 
 	server.Router.Mount("/todos", todosRouter)
 }
 
 func main() {
-	server := CreateServer()
+	db, err := DBClient()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	server := CreateServer(db)
 	server.MountHandlers()
 
 	fmt.Println("server running on port:5000")
-	http.ListenAndServe(":5000", server.Router)
+	http.ListenAndServe(PORT, server.Router)
 }
 
 type Todo struct {
+	Id        int       `json:"id"`
+	Task      string    `json:"task"`
+	Completed bool      `json:"completed"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type TodoRequestBody struct {
 	Task      string `json:"task"`
 	Completed bool   `json:"completed"`
 }
 
-var Todos []*Todo
-
-func AddTodo(w http.ResponseWriter, r *http.Request) {
+func scanRow(rows *sql.Rows) (*Todo, error) {
 	todo := new(Todo)
+	err := rows.Scan(&todo.Id,
+		&todo.Task,
+		&todo.Completed,
+		&todo.CreatedAt,
+		&todo.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return todo, nil
+}
+
+func (server *Server) AddTodo(w http.ResponseWriter, r *http.Request) {
+	todo := new(TodoRequestBody)
 	if err := json.NewDecoder(r.Body).Decode(todo); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("Please enter a correct Todo!!"))
 		return
 	}
-	Todos = append(Todos, todo)
+
+	query := `INSERT INTO Todos (task, completed) VALUES (?, ?)`
+	_, err := server.DB.Exec(query, todo.Task, todo.Completed)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Something bad happened on the server :("))
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Todo added!!"))
 }
 
-func GetTodos(w http.ResponseWriter, r *http.Request) {
+func (server *Server) GetTodos(w http.ResponseWriter, r *http.Request) {
+	query := `SELECT * FROM Todos ORDER BY created_at DESC`
+
+	rows, err := server.DB.Query(query)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Something bad happened on the server :("))
+		return
+	}
+
+	var todos []*Todo
+
+	for rows.Next() {
+		todo, err := scanRow(rows)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Something bad happened on the server :("))
+			return
+		}
+		todos = append(todos, todo)
+	}
+
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(Todos)
+	json.NewEncoder(w).Encode(todos)
 }
